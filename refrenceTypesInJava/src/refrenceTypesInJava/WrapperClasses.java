64c57bfb19ef04ca2623c7c276d965d277889def
kapil-sharma99 <ks2822123@gmail.com>
2021-06-29 19:11:28 +0530

    Refrence Types in Java started

---
+++ refrenceTypesInJava/src/refrenceTypesInJava/WrapperClasses.java
@@ -0,0 +1,13 @@
+package refrenceTypesInJava;
+
+public class WrapperClasses {
+
+	public static void main(String[] args) {
+		int i = 5;
+		Integer integer = Integer.valueOf(5);
+		Integer integer1 = Integer.valueOf(5);
+		
+		Character character = Character.valueOf('K');
+	}
+
+}
