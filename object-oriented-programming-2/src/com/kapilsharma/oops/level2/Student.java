3cef8c282ca3917f52411eb2b6b21305169a2630
kapil-sharma99 <ks2822123@gmail.com>
2021-07-02 17:41:56 +0530

    OOP2: Inheritance started

---
+++ object-oriented-programming-2/src/com/kapilsharma/oops/level2/Student.java
@@ -0,0 +1,21 @@
+package com.kapilsharma.oops.level2;
+
+public class Student extends Person{
+	private String collegeName;
+	private int year;
+	
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+}
---
+++ object-oriented-programming-2/src/com/kapilsharma/oops/level2/StudentRunner.java
@@ -0,0 +1,11 @@
+package com.kapilsharma.oops.level2;
+
+public class StudentRunner {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.
+
+	}
+
+}
