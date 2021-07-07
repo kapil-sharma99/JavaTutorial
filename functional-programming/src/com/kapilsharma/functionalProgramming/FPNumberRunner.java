b7d97a534e970b61ba6a28109915d3d059dda440
kapil-sharma99 <ks2822123@gmail.com>
2021-07-07 19:33:42 +0530

    functional Programming continued

--- functional-programming/src/com/kapilsharma/functionalProgramming/FPNumberRunner.java
+++ functional-programming/src/com/kapilsharma/functionalProgramming/FPNumberRunner.java
@@ -5,15 +5,23 @@ import java.util.List;
 public class FPNumberRunner {
 
 	public static void main(String[] args) {
-		List<Integer> numbers = List.of(4, 6, 8, 13, 3, 5);
+		List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
 		numbers.stream().forEach(element -> System.out.println(element));
 		
-		 int reduced = numbers.stream().reduce(0, (number1, number2) -> number1+number2);
-		 System.out.println(reduced);
+		 FPSum(numbers);
+		 
+		 numbers.stream().sorted().forEach(e->System.out.println(e)); //sorting using fp
+		 numbers.stream().distinct().forEach(e->System.out.println(e));
 		
 		int sum = normalSum(numbers);
 		System.out.println(sum);
 	}
+
+	private static void FPSum(List<Integer> numbers) {
+		int reduced = numbers.stream().reduce(0, (number1, number2) -> number1+number2);
+		 System.out.println(reduced);
+	}
+	
 
 	private static int normalSum(List<Integer> numbers) {
 		int sum = 0;
