cf4ab05be7f7817986046997889928929de659f2
kapil-sharma99 <ks2822123@gmail.com>
2021-07-08 20:10:54 +0530

    Threads created for task1 and task2

--- Multithreads/src/com/kapilsharma/multithread/ThreadBasicsRunner.java
+++ Multithreads/src/com/kapilsharma/multithread/ThreadBasicsRunner.java
@@ -6,8 +6,21 @@ class Task1 extends Thread {
 		for(int i = 101; i < 199; i++) {
 			System.out.print(i + " ");
 		}
-		System.out.print("\n Task1 completed");
+		System.out.print("\nTask1 completed");
 	}
+}
+
+class Task2 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.print("\nTask2 Started");
+		for(int i = 201; i < 299; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.print("\nTask2 completed");
+	}
+	
 }
 
 public class ThreadBasicsRunner {
@@ -18,17 +31,16 @@ public class ThreadBasicsRunner {
 		task1.start();
 		
 		//Task2
-		for(int i = 201; i <= 299; i++) {
-			System.out.print(i + " ");
-		}
-		System.out.println("\n Task 2 is done!!");
+		Task2 task2 = new Task2();
+		Thread task2Thread = new Thread(task2);
+		task2Thread.start();
 		
 		//Task3
 		for(int i = 301; i <= 399; i++) {
 			System.out.print(i + " ");
 		}
-		System.out.println("\n Task 3 is done!!");
-		System.out.println("\n Main Method done is done!!");
+		System.out.println("\nTask 3 is done!!");
+		System.out.println("\nMain Method done is done!!");
 	}
 
 }
