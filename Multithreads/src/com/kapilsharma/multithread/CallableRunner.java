60d3b1e81e456e7b1821b613f8707aa5dc44ce35
kapil-sharma99 <ks2822123@gmail.com>
2021-07-08 21:43:36 +0530

    Threads executor and more than 2 executor and return value

---
+++ Multithreads/src/com/kapilsharma/multithread/CallableRunner.java
@@ -0,0 +1,36 @@
+package com.kapilsharma.multithread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class CallableTask implements Callable<String> {
+	
+	String name;
+	
+	public CallableTask(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return "Hello" + this.name;
+	}
+	
+}
+
+public class CallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService exe = Executors.newFixedThreadPool(1);
+		Future<String> welcomeFuture = exe.submit(new CallableTask("Kapil Sharma"));
+		System.out.print("\nnew CallableTask(\"Kapil Shatma\") executed");
+		String welcomeMsg = welcomeFuture.get();
+		System.out.println(welcomeMsg);
+	}
+
+}
