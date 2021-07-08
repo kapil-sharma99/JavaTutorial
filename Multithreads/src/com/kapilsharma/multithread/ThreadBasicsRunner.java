378daed3c5a60876330ee0b296ae5fe4af0411dd
kapil-sharma99 <ks2822123@gmail.com>
2021-07-08 19:08:05 +0530

    Threads created for task1

--- Multithreads/src/com/kapilsharma/multithread/ThreadBasicsRunner.java
+++ Multithreads/src/com/kapilsharma/multithread/ThreadBasicsRunner.java
@@ -1,14 +1,21 @@
 package com.kapilsharma.multithread;
 
+class Task1 extends Thread {
+	public void run() {
+		System.out.print("\nTask1 Started");
+		for(int i = 101; i < 199; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.print("\n Task1 completed");
+	}
+}
+
 public class ThreadBasicsRunner {
 
 	public static void main(String[] args) {
 		//Task1
-		for(int i = 101; i <= 199; i++) {
-			System.out.print(i + " ");
-		}
-		
-		System.out.println("\n Task 1 is done!!");
+		Task1 task1 = new Task1();
+		task1.start();
 		
 		//Task2
 		for(int i = 201; i <= 299; i++) {
