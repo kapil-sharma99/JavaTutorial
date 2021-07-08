929f956c6562d8c9605e1658553c7c29daa07099
kapil-sharma99 <ks2822123@gmail.com>
2021-07-08 21:23:54 +0530

    Threads executor

---
+++ Multithreads/src/com/kapilsharma/multithread/ExecutorServiceRunner.java
@@ -0,0 +1,13 @@
+package com.kapilsharma.multithread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceRunner {
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		executorService.execute(new Task1());
+		executorService.execute(new Thread(new Task2()));
+		executorService.shutdown();
+	}
+}
