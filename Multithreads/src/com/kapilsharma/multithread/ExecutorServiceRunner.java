aa15fa45346f7dd19eb6283c9c0d42f55eb563bd
kapil-sharma99 <ks2822123@gmail.com>
2021-07-08 21:35:56 +0530

    Threads executor and more than 2 executor

--- Multithreads/src/com/kapilsharma/multithread/ExecutorServiceRunner.java
+++ Multithreads/src/com/kapilsharma/multithread/ExecutorServiceRunner.java
@@ -3,11 +3,31 @@ package com.kapilsharma.multithread;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task extends Thread {
+	int number;
+	
+	public Task(int number) {
+		this.number = number;
+	}
+	public void run() {
+		System.out.print("\nTask" + number + "Started");
+		for(int i = number*100; i < number*100+99; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.print("\nTask" + number + "completed");
+	}
+}
+
 public class ExecutorServiceRunner {
 	public static void main(String[] args) {
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		executorService.execute(new Task1());
-		executorService.execute(new Thread(new Task2()));
+//		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		ExecutorService executorService = Executors.newFixedThreadPool(4);
+//		executorService.execute(new Task1());
+//		executorService.execute(new Thread(new Task2()));
+		executorService.execute(new Task(1));
+		executorService.execute(new Task(2));
+		executorService.execute(new Task(3));
+		executorService.execute(new Task(4));
 		executorService.shutdown();
 	}
 }
