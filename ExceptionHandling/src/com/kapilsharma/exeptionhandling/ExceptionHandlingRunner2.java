d4eabe26e274d06355ef2bb8ffdc9adb33e17e25
kapil-sharma99 <ks2822123@gmail.com>
2021-07-09 19:46:03 +0530

    null point and arrayIndexOutOfBounds exception

--- ExceptionHandling/src/com/kapilsharma/exeptionhandling/ExceptionHandlingRunner2.java
+++ ExceptionHandling/src/com/kapilsharma/exeptionhandling/ExceptionHandlingRunner2.java
@@ -13,11 +13,19 @@ public class ExceptionHandlingRunner2 {
 
 	private static void method2() {
 		try {
-			String str = null;
-			str.length();
+//			String str = null;
+//			str.length();
+			int[] i = {1, 3};
+			int number = i[3];
 			System.out.println("Method 2!!");
+		} catch(NullPointerException ex) {
+			System.out.println("Null Pointer Exception...");
+			ex.printStackTrace();
+		} catch(ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Array Index out of bounds Exception...");
+			ex.printStackTrace();
 		} catch(Exception ex) {
 			ex.printStackTrace();
-		}
+		} 
 	}
 }
