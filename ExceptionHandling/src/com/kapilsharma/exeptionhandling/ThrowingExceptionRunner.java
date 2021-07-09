6f602c23fddbaa7feaf75069bb353cb8de9b65e4
kapil-sharma99 <ks2822123@gmail.com>
2021-07-09 20:22:03 +0530

    Throwing new exception

--- ExceptionHandling/src/com/kapilsharma/exeptionhandling/ThrowingExceptionRunner.java
+++ ExceptionHandling/src/com/kapilsharma/exeptionhandling/ThrowingExceptionRunner.java
@@ -1,10 +1,33 @@
 package com.kapilsharma.exeptionhandling;
 
+class Amount {
+	private String currency;
+	private int amount;
+	public Amount(String currency, int amount) {
+		super();
+		this.currency = currency;
+		this.amount = amount;
+	}
+	
+	public void add(Amount other) {
+		if(!this.currency.equals(other.currency)) {
+			throw new RuntimeException("Currencies dont matched");
+		}
+		this.amount = this.amount + other.amount;
+	}
+	
+	public String toString() {
+		return amount + " " + currency;
+	}
+}
+
 public class ThrowingExceptionRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Amount amount1 = new Amount("USD", 10);
+		Amount amount2 = new Amount("INR", 20);
+		amount1.add(amount2);
+		System.out.println(amount1);
 	}
 
 }
