ea6cba2c51747eb430ae697830ce0d9b479bbbf0
kapil-sharma99 <ks2822123@gmail.com>
2021-07-09 20:28:29 +0530

    Throwing new exception in a better way

--- ExceptionHandling/src/com/kapilsharma/exeptionhandling/ThrowingExceptionRunner.java
+++ ExceptionHandling/src/com/kapilsharma/exeptionhandling/ThrowingExceptionRunner.java
@@ -9,9 +9,9 @@ class Amount {
 		this.amount = amount;
 	}
 	
-	public void add(Amount other) {
+	public void add(Amount other) throws Exception{
 		if(!this.currency.equals(other.currency)) {
-			throw new RuntimeException("Currencies dont matched");
+			throw new Exception("Currencies dont matched");
 		}
 		this.amount = this.amount + other.amount;
 	}
@@ -23,7 +23,7 @@ class Amount {
 
 public class ThrowingExceptionRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Amount amount1 = new Amount("USD", 10);
 		Amount amount2 = new Amount("INR", 20);
 		amount1.add(amount2);
