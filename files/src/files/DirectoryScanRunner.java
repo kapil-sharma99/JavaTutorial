07201a156bf1eb79ba4929ffe987a16188baebc1
kapil-sharma99 <ks2822123@gmail.com>
2021-07-14 20:07:51 +0530

    files started and 4th lecture done

--- files/src/files/DirectoryScanRunner.java
+++ files/src/files/DirectoryScanRunner.java
@@ -9,11 +9,9 @@ import java.util.List;
 public class DirectoryScanRunner {
 
 	public static void main(String[] args) throws IOException {
-		Path pathFileToRead = Paths.get("./resources/data.txt");
-//		List<String> lines = Files.readAllLines(pathFileToRead);
-//		System.out.println(lines);
-		
-		Files.lines(pathFileToRead).forEach(System.out::println); //better way bcoz returns stream
+		Path pathFileToWrite = Paths.get("./resources/file-write.txt");
+		List<String> list = List.of("Apple", "Bat", "Cat");
+		Files.write(pathFileToWrite, list);
 	}
 
 }
