ca9ae900640b4aa2e2b96eab4a082eab64ba82f7
kapil-sharma99 <ks2822123@gmail.com>
2021-07-14 20:04:38 +0530

    files started and 3rd lecture done

--- files/src/files/DirectoryScanRunner.java
+++ files/src/files/DirectoryScanRunner.java
@@ -4,21 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
+import java.util.List;
 
 public class DirectoryScanRunner {
 
 	public static void main(String[] args) throws IOException {
-		Path currentDirectory = Paths.get(".");
-		//Files.list(currentDirectory).forEach(System.out::println);
+		Path pathFileToRead = Paths.get("./resources/data.txt");
+//		List<String> lines = Files.readAllLines(pathFileToRead);
+//		System.out.println(lines);
 		
-		Predicate<? super Path> predicate = path -> String.valueOf(path).contains(".java");
-		Files.walk(currentDirectory, 4).filter(predicate ).forEach(System.out::println);
-		
-		BiPredicate<Path, BasicFileAttributes> matcher = (path, attributes) -> String.valueOf(path).contains(".java");
-		Files.find(currentDirectory, 4, matcher).forEach(System.out::println);
+		Files.lines(pathFileToRead).forEach(System.out::println); //better way bcoz returns stream
 	}
 
 }
