bfe31cc880d2eb73d1c34a5ee4a898d9d41fd527
kapil-sharma99 <ks2822123@gmail.com>
2021-07-15 17:57:19 +0530

    tips and tricks: lecture 2 completed

---
+++ tips-and-tricks/src/com/kapilsharma/tips/blocks/BlocksRunner.java
@@ -0,0 +1,11 @@
+package com.kapilsharma.tips.blocks;
+
+public class BlocksRunner {
+
+	public static void main(String[] args) {
+		if(3>2) {
+			System.out.println("3>2");
+		} //anything inside there brackets are block
+	}
+
+}
