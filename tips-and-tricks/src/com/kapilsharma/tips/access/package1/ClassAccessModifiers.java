a63a330cea53fe28215f2353c13ad00e06b49c42
kapil-sharma99 <ks2822123@gmail.com>
2021-07-15 18:46:18 +0530

    tips and tricks: lecture 5 completed

---
+++ tips-and-tricks/src/com/kapilsharma/tips/access/package1/ClassAccessModifiers.java
@@ -0,0 +1,9 @@
+package com.kapilsharma.tips.access.package1;
+
+public class ClassAccessModifiers {
+
+	public static void main(String[] args) {
+		ClassAccessModifiers access = new ClassAccessModifiers();
+	}
+
+}
---
+++ tips-and-tricks/src/com/kapilsharma/tips/access/package2/ClassAccessModifiersRunnerInOtherPackage.java
@@ -0,0 +1,11 @@
+package com.kapilsharma.tips.access.package2;
+
+import com.kapilsharma.tips.access.package1.ClassAccessModifiers;
+
+public class ClassAccessModifiersRunnerInOtherPackage {
+
+	public static void main(String[] args) {
+		ClassAccessModifiers c = new ClassAccessModifiers();
+	}
+
+}
