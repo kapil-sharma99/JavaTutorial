3fd44ac24248ffc7224b7e69aae2e5ec559176bb
kapil-sharma99 <ks2822123@gmail.com>
2021-07-15 17:54:09 +0530

    tips and tricks: lecture 1 completed

--- tips-and-tricks/src/com/kapilsharma/tips/imports/ImportRunner.java
+++ tips-and-tricks/src/com/kapilsharma/tips/imports/ImportRunner.java
@@ -1,10 +1,18 @@
 package com.kapilsharma.tips.imports;
 
+import static java.lang.System.out;
+import static java.util.Collections.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class ImportRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		out.println("Hello world");
+		
+		sort(new ArrayList<String>());
+		
+		BigDecimal bd = null;
 	}
 
 }
