56fdfa1aaf76ac4d61f577985d468e83bb72db4e
kapil-sharma99 <ks2822123@gmail.com>
2021-07-19 19:24:06 +0530

    tips and tricks: lecture 7 completed

--- tips-and-tricks/src/com/kapilsharma/tips/access/package1/FinalNonAccessModifierRunner.java
+++ tips-and-tricks/src/com/kapilsharma/tips/access/package1/FinalNonAccessModifierRunner.java
@@ -1,7 +1,15 @@
 package com.kapilsharma.tips.access.package1;
 
-final class FinalClass { //final class cannot be extended further.
-	
+final class FinalClass {}; //final class cannot be extended further
+
+class SomeClass {
+	final public void doSomething() {
+		
+	}
+}
+
+class ExtendingClass extends SomeClass {
+	//public void doSomething() {}; [connot be modified because final keyword added]
 }
 
 public class FinalNonAccessModifierRunner {
