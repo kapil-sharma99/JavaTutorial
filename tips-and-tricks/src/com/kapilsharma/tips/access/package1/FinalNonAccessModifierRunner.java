5b431eede7e1b0c15f0fbc77cd97cd4158681e7e
kapil-sharma99 <ks2822123@gmail.com>
2021-07-19 19:13:46 +0530

    tips and tricks: lecture 7 started

---
+++ tips-and-tricks/src/com/kapilsharma/tips/access/package1/FinalNonAccessModifierRunner.java
@@ -0,0 +1,14 @@
+package com.kapilsharma.tips.access.package1;
+
+final class FinalClass { //final class cannot be extended further.
+	
+}
+
+public class FinalNonAccessModifierRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
