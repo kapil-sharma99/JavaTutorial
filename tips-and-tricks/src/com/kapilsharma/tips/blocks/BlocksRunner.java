1024810c7ddb903fea36a8743bc7f2d39a882bec
kapil-sharma99 <ks2822123@gmail.com>
2021-07-19 21:14:16 +0530

    tips and tricks: lecture 12 completed

--- tips-and-tricks/src/com/kapilsharma/tips/blocks/BlocksRunner.java
+++ tips-and-tricks/src/com/kapilsharma/tips/blocks/BlocksRunner.java
@@ -1,11 +1,13 @@
 package com.kapilsharma.tips.blocks;
 
 public class BlocksRunner {
+	
+	public static final int SECONDS_IN_MINUTE = 60;
+	public static final int MINUTES_IN_HOURS = 60;
+	public static final int HOURS_IN_DAY = 24;
+	public static final int SECONDS_IN_DAY = SECONDS_IN_MINUTE * MINUTES_IN_HOURS * HOURS_IN_DAY;
 
 	public static void main(String[] args) {
-		if(3>2) {
-			System.out.println("3>2");
-		} //anything inside there brackets are block
+		System.out.println(SECONDS_IN_DAY);
 	}
-
 }
