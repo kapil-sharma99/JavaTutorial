0ac77271374421fa7d51ca73fe484d79fcdb001c
kapil-sharma99 <ks2822123@gmail.com>
2021-07-20 21:30:32 +0530

    tips and tricks: lecture 12 completed & static nested class Updated

---
+++ tips-and-tricks/src/com/kapilsharma/tips/nestedclasses/NestedClassRunner.java
@@ -0,0 +1,22 @@
+package com.kapilsharma.tips.nestedclasses;
+
+class Person {};
+
+public class NestedClassRunner {
+	
+	class InnerClass {
+		
+	}
+	
+	static class StaticNestedClass {
+		public StaticNestedClass() {
+			System.out.println("Static Nested Class");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
